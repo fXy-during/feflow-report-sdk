26106e665d412a55fd10a1f5c9e31e58d1a39c43
fXy-during <fxy1044667399@163.com>
2019-11-25 11:46:45 +0800

    fix: 修复user_name和proxy问题

--- src/dev.ts
+++ src/dev.ts
@@ -1,5 +1,29 @@
-const Report = require('./index');
+// const Report = require('./index');
+// const Http = require('http');
+// const report = new Report({});
+import URI from './api/config';
 
-const report = new Report({});
+import rp from 'request-promise';
 
-report.report('dev');
+// report.report('dev');
+
+// Http.request({
+//   host: 'http://127.0.0.1:12639',
+//   path: 'http://193.112.196.252/api/v1/report/command',
+//   method: 'post',
+
+// });
+
+rp({
+  method: 'POST',
+  uri: URI.REPORT_URL,
+  body: { command: 'dev' },
+  json: true,
+  proxy: URI.REPORT_PROXY
+})
+  .then(res => {
+    console.log('got report response', res);
+  })
+  .catch(e => {
+    console.log('feflow report fail', e);
+  });
